1df22f1221c73dcd63cc468357989b2aebcd1320
Colt Frederickson <coltfred@gmail.com>
2024-11-22 12:21:13 -0700

    formatting

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,6 +1,6 @@
 { pkgs, config, username, nixpkgs, nixpkgs-unstable, ... }: {
   time.timeZone = "America/Denver";
-    # Fixes error about home dir being /var/empty
+  # Fixes error about home dir being /var/empty
   # See https://github.com/nix-community/home-manager/issues/4026
   users.users.${username} = {
     home =
